b0fce1c19275e66348dcfe9d741622967220f958
Shah Inayath <128777051+Virtual-Git@users.noreply.github.com>
2026-07-21 09:36:40 +0530

    Fix Python module resolution by dynamically appending api folder to sys.path

--- api/index.py
+++ api/index.py
@@ -1,9 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from api.routes import router as api_router
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
